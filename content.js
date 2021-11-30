b4d063de034b3327cee9ada6dc0a9d3264b2be07
Evan Sellers <github@sellersew.com>
2021-11-30 13:34:40 -0500

    broke off CSS

--- content.js
+++ content.js
@@ -1,3 +1,17 @@
+/*
+ *   Copyright (C) 2021 Sellers Industry
+ *   distributed under the MIT License
+ *
+ *   author: Evan Sellers <sellersew@gmail.com>
+ *   date: Tue Nov 30 2021
+ *   file: content.js
+ *   project: Moodle Custom Menu
+ *   purpose: Edits HTML on page
+ *
+ */
+
+
+
 let dummyFill = "<div style='text-align:center; padding-top: 4rem; padding-bottom: 4rem;'><b>Custom Moodle Menu</b><br>Designed by <a href='https://sellersew.com' target='blank'>Sellers Industry</a><br><br>Add menu items through the extension options page.<br><a href='/my'>My Dashboard</a></div>"
 
 function buildHtmlDom( items ) {
